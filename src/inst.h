2bdfc5ac3116c00e5b38ebf4f1196a2aee239165
August Herron <66635096+augustryder@users.noreply.github.com>
2024-11-24 23:28:48 -0600

    Revert "working"
    This reverts commit bf256e37fda9e9216871dcd39bc967f63d383ede.

--- src/inst.h
+++ src/inst.h
@@ -7,7 +7,7 @@ typedef struct {
     int vr; // virtual register
     int pr; // physical register
     int nu; // index of vrs next use
-    int ns; // index of next store
+    int ns; // index of next store inst
 } Operand;
 
 typedef struct {
