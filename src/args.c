1d2e1459167e984e337ee8e68c9b7c6660fb684b
August Herron <66635096+augustryder@users.noreply.github.com>
2024-11-30 21:51:32 -0600

    updated -h flag

--- src/args.c
+++ src/args.c
@@ -120,13 +120,17 @@ void help(void) {
     fprintf(stdout, "Usage: ");
     fprintf(stdout, "%s [options] input file\n\n", __PROGRAM_NAME__);
     fprintf(stdout, "Options:\n\n");
-    fprintf(stdout, "\t-l, --lexer=-\n" 
-                    "\t\tPrints out the stream of tokens.\n\n");
-    fprintf(stdout, "\t-p, --pretty-print\n" 
-                    "\t\tPrints legal ILOC code.\n\n");
+    fprintf(stdout, "\t-a, --alloc\n" 
+                    "\t\tAllocates the input block of code. Pretty-prints the results by default.\n\n");
+    fprintf(stdout, "\t-k n\n" 
+                    "\t\tUse n registers when allocating the code.\n\n");
+    fprintf(stdout, "\t-l, --lexer\n" 
+                    "\t\tPrints out the stream of tokens from the input block of code.\n\n");
+    fprintf(stdout, "\t-p n, --pretty-print\n" 
+                    "\t\tPrints legal ILOC code. \n\t\t n = 0, prints source registers\n\t\t n = 1, prints virtual regisers\n\t\t n = 2, prints physical registers\n");
     fprintf(stdout, "\t-t, --table-print\n" 
                     "\t\tPrints the intermediate representation in tabular form.\n\n");
-    fprintf(stdout, "\t-dn, --version=-\n" 
+    fprintf(stdout, "\t-d n, --debug\n" 
                     "\t\tSets the debug level to n.\n\n");
     fprintf(stdout, "\t-h, --help\n" 
                     "\t\tPrints this help message.\n\n");
